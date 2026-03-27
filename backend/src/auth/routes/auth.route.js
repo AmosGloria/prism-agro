664ee4e67b327a15e80f6d0d11453cded2697f26
unknown <richkole8@gmail.com>
2026-03-27 02:20:06 +0100

    added log in

--- backend/src/auth/routes/auth.route.js
+++ backend/src/auth/routes/auth.route.js
@@ -2,11 +2,12 @@
 
 const express = require('express');
 const router = express.Router();
-const { registerUser, verifyOtp, resendOtp } = require('../controller/user.controller');
+const { registerUser, verifyOtp, resendOtp, logIn } = require('../controller/user.controller');
 
 router.post('/register', registerUser);
 router.post("/verifyOtp", verifyOtp);
 router.post("/resendOtp", resendOtp);
+router.post("/login", logIn)
 
 const userRoutes = router;
 
